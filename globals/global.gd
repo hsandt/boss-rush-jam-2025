11a006260a819f01680bd6cd3a782869756ca1ce
hsandt <1067772+hsandt@users.noreply.github.com>
2025-01-06 17:10:42 +0100

    [Global] Press Ctrl+Q (Windows, Linux) or Cmd+Q (macOS) or Escape to quit game immediately

--- globals/global.gd
+++ globals/global.gd
@@ -1,10 +1,13 @@
 extends Node
 
 func _process(_delta):
-	
+
 	if Input.is_action_just_pressed("toggle_fullscreen"):
 		toggle_fullscreen()
-		
+
+	if Input.is_action_just_pressed("exit_game"):
+		get_tree().quit()
+
 
 func toggle_fullscreen():
 	if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
