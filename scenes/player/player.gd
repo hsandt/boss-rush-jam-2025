9f15e8822ad5495019a8a9bfaff05a0c9f70e0b3
hsandt <1067772+hsandt@users.noreply.github.com>
2025-01-06 14:50:43 +0100

    [Player] Melee: plug Melee attack stub (only prints "melee")
    - Add Melee cancel timer with tunable Melee cancel time (0.5s) to chain with another melee attack or any action
    - Add tunable Melee attack damage (1.0, unused)
    
    Refactoring:
    - Replace can_ flags with can_ methods that check dashing (renamed is_dashing), as well as timer states (if cooldown and cancel timers are over)
    - In _process, check new can_control_move() just for the move check to be more modular. We still prevent shoot/melee during dash (not in control) but it will be easy to change if we want.
    
    Also, we allow to chain dash is cooldown < dash duration but only same direction.
    
    Input: add inputs:
    - arrows to move
    - ZXC to shoot/dash/melee attack
    - also right-click to melee attack

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -11,6 +11,8 @@ extends CharacterBody2D
 #@export_group("Shooting")
 @export var shoot_cooldown := 0.2
 @export var shoot_kb := 400.0
+@export var melee_attack_damage := 1.0
+@export var melee_cancel_time := 0.5
 
 # look direction
 var direction := Vector2.RIGHT
@@ -20,14 +22,18 @@ var input_dir := Vector2.ZERO
 var movt_dir := Vector2.RIGHT
 var moving := false
 var in_control := true
-var dashing := false
-var can_dash := true
-var can_shoot := true
+var is_dashing := false
+
+@onready var dash_for_timer: Timer = $Timers/Dash/For
+@onready var dash_cooldown_timer: Timer = $Timers/Dash/Cooldown
+@onready var shoot_cooldown_timer: Timer = $Timers/Shoot/Cooldown
+@onready var melee_cancel_timer: Timer = $Timers/Melee/Cancel
 
 func _ready():
-	$Timers/Dash/For.wait_time = dash_for
-	$Timers/Dash/Cooldown.wait_time = dash_cooldown
-	$Timers/Shoot/Cooldown.wait_time = shoot_cooldown
+	dash_for_timer.wait_time = dash_for
+	dash_cooldown_timer.wait_time = dash_cooldown
+	shoot_cooldown_timer.wait_time = shoot_cooldown
+	melee_cancel_timer.wait_time = melee_cancel_time
 
 func _process(delta):
 	get_input()
@@ -41,7 +47,7 @@ func _physics_process(delta):
 
 func move(delta):
 
-	if dashing:
+	if is_dashing:
 		velocity = dash_speed * movt_dir * delta
 	elif moving:
 		velocity.x = move_toward(velocity.x, movt_dir.x*max_speed, acceleration*delta)
@@ -56,52 +62,68 @@ func get_input():
 
 	direction = (get_global_mouse_position() - position).normalized()
 
-	if not in_control:
-		return
+	if can_control_move():
+		input_dir = Vector2.ZERO
+		if Input.is_action_pressed("left"):
+			input_dir.x -= 1
+		if Input.is_action_pressed("right"):
+			input_dir.x += 1
+		if Input.is_action_pressed("up"):
+			input_dir.y -= 1
+		if Input.is_action_pressed("down"):
+			input_dir.y += 1
 
-	input_dir = Vector2.ZERO
-	if Input.is_action_pressed("left"):
-		input_dir.x -= 1
-	if Input.is_action_pressed("right"):
-		input_dir.x += 1
-	if Input.is_action_pressed("up"):
-		input_dir.y -= 1
-	if Input.is_action_pressed("down"):
-		input_dir.y += 1
+		moving = not input_dir.is_zero_approx()
+		if moving:
+			input_dir = input_dir.normalized()
+			movt_dir = input_dir
 
-	moving = not input_dir.is_zero_approx()
-	if moving:
-		input_dir = input_dir.normalized()
-		movt_dir = input_dir
-
-	if can_dash and Input.is_action_just_pressed("dash"):
+	if can_dash() and Input.is_action_just_pressed("dash"):
 		dash()
 
-	if can_shoot and Input.is_action_just_pressed("shoot"):
+	if can_shoot() and Input.is_action_just_pressed("shoot"):
 		shoot()
+
+	if can_melee_attack() and Input.is_action_just_pressed("melee"):
+		melee_attack()
+
+func can_shoot():
+	return shoot_cooldown_timer.is_stopped() and not is_dashing
 
 func shoot():
 	print("shot")
-	can_shoot = false
-	$Timers/Shoot/Cooldown.start()
+	shoot_cooldown_timer.start()
 	# recoil / knockback
 	velocity -= direction * shoot_kb
 
+func can_melee_attack():
+	return melee_cancel_timer.is_stopped() and not is_dashing
+
+func melee_attack():
+	print("melee_attack")
+	melee_cancel_timer.start()
+
+func can_control_move():
+	return not is_dashing
+
+func can_dash():
+	return dash_cooldown_timer.is_stopped()
+
 func dash():
-	dashing = true
+	# if dash for cooldown < dash for time, player can chain dashes (only in same direction for now)
+	# before the last one has even finished (see can_dash)
+	# so it's important to stop the dash for timer from previous dash
+	# so it can properly restart now
+	if not dash_for_timer.is_stopped():
+		dash_for_timer.stop()
+
+	is_dashing = true
 	in_control = false
-	can_dash = false
-	$Timers/Dash/For.start()
-	$Timers/Dash/Cooldown.start()
+	dash_for_timer.start()
+	dash_cooldown_timer.start()
 
 func _on_dash_for_timeout():
-	dashing = false
+	is_dashing = false
 	in_control = true
 	if velocity.length_squared() > max_speed**2:
 		velocity = velocity.normalized() * max_speed
-
-func _on_dash_cooldown_timeout():
-	can_dash = true
-
-func _on_shoot_cooldown_timeout():
-	can_shoot = true
