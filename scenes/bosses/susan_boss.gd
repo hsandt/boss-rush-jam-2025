03260d5966f422f4afc0ac987be2cca29ee7ab5b
VantaTree <VantaTree@gmail.com>
2025-01-10 12:01:47 +0530

    added hurt box to boss + spin progress indicator

--- scenes/bosses/susan_boss.gd
+++ scenes/bosses/susan_boss.gd
@@ -5,30 +5,43 @@ extends Node2D
 ## 1.0: clockwise, -1.0: anticlockwise
 @export_range(-1.0, 1.0) var arm_rotation_modifier := 1.0
 @export_group("ArmStagger")
-@export var arm_stagger_time: = 1.0
-@export var enable_arm_shake_on_hit: = true
+@export var arm_stagger_time := 1.0
+@export var enable_arm_shake_on_hit := true
 @export var arm_shake_offset := 1.0
 @export var arm_shake_freq := 10.0
 @export var force_stop_arm := false
+@export_group("Spin")
+@export var max_spin := 3.0*TAU
+var spin_progress := 0.0
 
 @onready var arm_stagger_timer:Timer = $Timers/Arm/Stagger
+@onready var boss_spin_progress = $BossSpinProgress
 
 func _ready():
 	arm_stagger_timer.wait_time = arm_stagger_time
 
-func _physics_process(delta):
+func _process(_delta):
 
-	if not is_arm_staggered():
-		rotate_arm(delta)
+	update_boss_spin_ui()
 
 	# TODO placeholder for testing:
 	# to be removed later
 	if Input.is_action_just_pressed("shoot"):
 		provoke_arm_rotation_direction_reversal()
 
+func update_boss_spin_ui():
+	boss_spin_progress.material.set_shader_parameter("progress", spin_progress/max_spin)
+
+func _physics_process(delta):
+
+	if not is_arm_staggered():
+		rotate_arm(delta)
+
 ## rotates arm
 func rotate_arm(delta):
-	$Arm.rotate(arm_speed * arm_rotation_modifier * delta)
+	var angle = arm_speed * arm_rotation_modifier * delta
+	spin_progress += angle
+	$Arm.rotate(angle)
 
 func shake_arm():
 	var tween = create_tween()
@@ -54,7 +67,7 @@ func set_arm_modifier(value:float):
 func reset_arm_modifier():
 	arm_rotation_modifier = sigmoid(arm_rotation_modifier)
 
-func _on_player_hurt_area_body_entered(_body):
+func _on_player_hurt_arm_area_body_entered(_body):
 	print("player hit arm")
 	# TODO make the player take damage, knockback & stagger
 	#body.stagger()
@@ -63,6 +76,11 @@ func _on_player_hurt_area_body_entered(_body):
 	if enable_arm_shake_on_hit:
 		shake_arm()
 	$Arm/AnimationPlayer.play("RESET")
+
+func _on_player_hurt_body_area_body_entered(_body):
+	print("player hit boss's body")
+	#TODO player hit with minor stagger
+	#TODO slight kb away from the boss and arm
 
 ## sigmoid math function
 func sigmoid(value:float) -> float:
