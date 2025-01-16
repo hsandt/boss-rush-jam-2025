7e400a9b06682cf54e26027ec66b7e8b5bbba3dc
VantaTree <VantaTree@gmail.com>
2025-01-16 12:21:19 +0530

    upgraded projectile group system + added a new projectile pattern

--- scenes/bosses/susan_projectile_manager.gd
+++ scenes/bosses/susan_projectile_manager.gd
@@ -1,10 +1,11 @@
 extends Node
 
 @export var spawn_delay := 5.0
-@export_range(1, 100) var group_count := 5
+@export_range(1, 100) var group_count := 3
 @export_range(0.0, 10.0) var group_burst_delay := 0.2
 
-@onready var proj_grp_tri_scene:PackedScene = preload("res://scenes/projectiles/projectile_groups/projectile_group_001.tscn")
+@onready var proj_grp_tri_scene001:PackedScene = preload("res://scenes/projectiles/projectile_groups/projectile_group_001.tscn")
+@onready var proj_grp_tri_scene002:PackedScene = preload("res://scenes/projectiles/projectile_groups/projectile_group_002.tscn")
 @onready var boss_owner:BaseBoss = $".."
 
 var time_value := 0.0
@@ -13,16 +14,25 @@ func _physics_process(delta):
 	time_value += delta
 
 func _on_proj_delay_001_timeout():
+
+	#TODO temporary pattern for testing groups
 	var tween = create_tween()
+	var which_proj_group = spawn_projectile_group001 if int(time_value)%2 else spawn_projectile_group002
 	for i in range(group_count):
-		tween.tween_callback(spawn_projectile_group)
+		tween.tween_callback(which_proj_group)
 		tween.tween_interval(group_burst_delay)
 
-func spawn_projectile_group():
-	var proj_group = proj_grp_tri_scene.instantiate()
-	var rotation_speed = 4.0
+func spawn_projectile_group001():
+	var proj_group = proj_grp_tri_scene001.instantiate() as BaseProjectileGroup
+	proj_group.rotation_speed = 4.0
 	#var direction = (boss_owner.player.position - boss_owner.position).normalized()
-	var direction = Vector2.RIGHT.rotated(time_value*3.0)
-	var speed = 200.0
-	proj_group.setup(rotation_speed, direction, speed)
+	proj_group.direction = Vector2.RIGHT.rotated(time_value*3.0)
+	proj_group.speed = 200.0
+	proj_group.expand_speed = proj_group.speed * 0.08
+	boss_owner.get_projectile(proj_group)
+
+func spawn_projectile_group002():
+	var proj_group = proj_grp_tri_scene002.instantiate() as BaseProjectileGroup
+	proj_group.rotation_speed = 1.0
+	proj_group.expand_speed = 200.0
 	boss_owner.get_projectile(proj_group)
