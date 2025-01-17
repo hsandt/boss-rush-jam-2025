f2249806c9ed791b04b38d204030461b6d866502
hsandt <1067772+hsandt@users.noreply.github.com>
2025-01-17 20:58:31 +0100

    [Projectile] Merged projectile hurtbox and hitbox (now named hitbox)
    - Merge all boss projectile layers, split player collision layer into player_body and player_hurtbox
    - Projectile still has 2 callbacks, but layer mask is done so that we are sure to only hit:
        - in area, player hurt box area (in theory melee hitbox too but that's still processed from player side)
        - in body, a wall (world layer)
    
    So previous behavior has been preserved

--- scenes/projectiles/base_projectile.gd
+++ scenes/projectiles/base_projectile.gd
@@ -1,29 +1,26 @@
 class_name BaseProjectile
 extends Node
 
-@export var is_destructible: = false:
-	set(value):
-		is_destructible = value
-		if hurt_box_area:
-			hurt_box_area.set_collision_layer_value(6, value)
+@export var can_be_hit_by_melee_attack: = false
 @export var projectile_attack_damage := 1.0
 
-#@onready var projectile_hit_box: Area2D = $HitBoxArea2D
-@onready var hurt_box_area:Area2D = $HurtBoxArea2D
-
 func _ready():
-	hurt_box_area.set_collision_layer_value(6, is_destructible)
+	pass
 
 func be_hurt_by_melee(_damage: float):
 	queue_free()
 
-func _on_hit_box_area_2d_area_entered(area):
+func _on_hit_box_area_2d_area_entered(area: Area2D):
+	# We set up collision mask so the only areas hit are player hurt box and player melee hitbox
+	# (but currently collision with the latter is verified from player side)
 	var player_hurt_box := area as PlayerHurtBox
 	if player_hurt_box:
 		player_hurt_box.be_hurt_by_projectile(projectile_attack_damage)
 		queue_free()
 
-func _on_hurt_box_area_2d_body_entered(_body):
+func _on_hit_box_area_2d_body_entered(body: Node2D):
+	# We set up collision mask so the only physics body hit is world,
+	# so we know we are hitting a wall
 	#TODO add destruction animation
 	#print("projectile, hit terrain/world")
 	queue_free()
