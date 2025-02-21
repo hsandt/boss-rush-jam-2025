f9c74ca0e86583b46c6e62c0c321fa2d9eb783c8
hsandt <1067772+hsandt@users.noreply.github.com>
2025-02-21 15:15:17 +0100

    [Menu] Main menu intro: the whole menu is spinning and scaling down then arrives with a shake for style and to evoke spin theme

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -1,11 +1,51 @@
 extends Control
 
+@export_range(0.0, 5*360, 0.01, "radians_as_degrees") var menu_intro_start_rotation := deg_to_rad(2.5*360)
+@export_range(0.0, 5.0, 0.01, "or_greater") var menu_intro_start_scale := 2.5
+@export_range(0.0, 5.0, 0.01, "or_greater") var menu_intro_duration := 1.0
+@export_range(0.0, 50.0, 0.01, "or_greater") var menu_intro_shake_amplitude := 50.0
+@export_range(0.0, 0.1, 0.01, "or_greater") var menu_intro_shake_period := 0.05
+@export_range(0.0, 0.25, 0.01, "or_greater") var menu_intro_shake_duration := 0.25
 @onready var menu_manager = $".."
 @onready var start_button = $VBoxContainer/StartButton
 
 func _ready():
+	play_main_menu_intro()
+
 	# doesnt work
 	start_button.grab_focus()
+
+func play_main_menu_intro():
+	# Vintage rotating/scaled down newspaper appear effect
+	var tween_appear = create_tween()
+	tween_appear.parallel().tween_property(self, ^"rotation", 0.0, menu_intro_duration) \
+		.from(menu_intro_start_rotation) \
+		.set_ease(Tween.EASE_IN).set_trans(Tween.TRANS_CUBIC)
+	tween_appear.parallel().tween_property(self, ^"scale", Vector2.ONE, menu_intro_duration) \
+		.from(menu_intro_start_scale * Vector2.ONE)
+
+	await tween_appear.finished
+
+	# Shake effect, unsyncing X and Y via randomness
+
+	var tween_shake_x = create_tween()
+	tween_shake_x.tween_property(self, ^"position:x", menu_intro_shake_amplitude * randf_range(0.5, 1.0), menu_intro_shake_period * randf_range(0.5, 1.0)) \
+		.from_current()
+	tween_shake_x.tween_property(self, ^"position:x", -menu_intro_shake_amplitude * randf_range(0.5, 1.0), menu_intro_shake_period * randf_range(0.5, 1.0))
+	tween_shake_x.set_loops(0)
+
+	var tween_shake_y = create_tween()
+	tween_shake_y.tween_property(self, ^"position:y", menu_intro_shake_amplitude * randf_range(0.5, 1.0), menu_intro_shake_period * randf_range(0.5, 1.0)) \
+		.from_current()
+
+	tween_shake_y.tween_property(self, ^"position:y", -menu_intro_shake_amplitude * randf_range(0.5, 1.0), menu_intro_shake_period * randf_range(0.5, 1.0))
+	tween_shake_y.set_loops(0)
+
+	# reset to center position
+	await get_tree().create_timer(menu_intro_shake_duration).timeout
+	tween_shake_x.stop()
+	tween_shake_y.stop()
+	position = Vector2.ZERO
 
 func _on_settings_button_pressed():
 	self.hide()
