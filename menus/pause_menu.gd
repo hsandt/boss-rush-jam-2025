4ee43602947fc1807b7adf44461f11583952e882
Hyper Sonic <1067772+hsandt@users.noreply.github.com>
2026-04-22 13:42:12 +0200

    [Pause] Focus resume on Pause menu open to support full gamepad/keyboard only support
    Also change "Quit" -> "Back to main menu" to be consistent with behavior

--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -2,9 +2,19 @@ extends MarginContainer
 
 @onready var in_game_manager: InGameManager = get_tree().get_first_node_in_group("in_game_manager")
 @onready var pause_menu_canvas_layer: CanvasLayer = $".."
+@onready var resume_button: Button = $VBoxContainer/ResumeButton
 
 func _on_resume_button_pressed():
 	in_game_manager.resume_game()
 	
 func _on_quit_button_pressed():
 	in_game_manager.go_back_to_menu()
+
+
+func _on_visibility_changed() -> void:
+	if not resume_button:
+		# too early, happens when loading scene
+		return
+		
+	if is_visible_in_tree():
+		resume_button.grab_focus()
